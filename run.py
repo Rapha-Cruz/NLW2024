20a4ebcfedb4751409a1d3d2bb0dc4a8946f4f92
Rapha-Cruz <raphajcruz@hotmail.com>
2024-07-11 16:51:28 -0300

    feat: third class

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 from src.main.server.server import app
+#importar o gerente de conexoes
+from src.models.settings.db_connection_handler import db_connection_handler
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
-
+    db_connection_handler.connect()
+    app.run(host="localhost", port=3000, debug=True)
